7916f7c9a6ff49373317acd93ccb4d48362dfd4f
phani-mithu <116943816+phani-mithu@users.noreply.github.com>
2022-11-01 19:14:42 +0530

    Create TwoSum.java

---
+++ Two sum/TwoSum.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Twosum {
+    public static void main(String[] args) {
+        int[] nums={2,7,11,15};
+        int target=18;
+        System.out.println(Arrays.toString(twoSum(nums,target)));
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> temp=new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            if(temp.containsKey(target-nums[i]))
+            return new int[]{temp.get(target-nums[i]),i};
+            else
+            temp.put(nums[i],i);
+        }
+        return null;
+    }
+}
