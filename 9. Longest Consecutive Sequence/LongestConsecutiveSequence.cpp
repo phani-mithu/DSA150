1b34a96b46c9a9cf5f29a9b3f647f0f36c1c6b78
phani-mithu <116943816+phani-mithu@users.noreply.github.com>
2022-11-04 00:36:14 +0530

    Update LongestConsecutiveSequence.cpp

--- 9. Longest Consecutive Sequence/LongestConsecutiveSequence.cpp
+++ 9. Longest Consecutive Sequence/LongestConsecutiveSequence.cpp
@@ -1,1 +1,27 @@
-
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin() , nums.end());
+        
+        int ans = 0, temp = 0;
+        int j = -1;
+        
+        for(int i = 0; i< n; i++){           
+            if(j != -1 && nums[i] - nums[j] == 1 ){
+               temp++; 
+               j = i;  
+            }else if( j != -1 && nums[i] - nums[j] == 0 ){
+               continue; 
+            }else{
+                ans = max(ans, temp);
+                temp = 1;
+                j = i;
+            }
+        }
+        
+        ans = max(ans, temp);
+        
+        return ans;
+    }
+};
