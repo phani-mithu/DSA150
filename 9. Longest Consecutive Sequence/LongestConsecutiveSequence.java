e4eeb404b91307d03bfa058f0c35f0c0e905cf91
phani-mithu <116943816+phani-mithu@users.noreply.github.com>
2022-11-03 20:15:01 +0530

    Create  LongestConsecutiveSequence.java

---
+++ 9. Longest Consecutive Sequence/LongestConsecutiveSequence.java
@@ -0,0 +1,35 @@
+package Days150;
+
+import java.util.Arrays;
+
+public class LongestConsecutiveSequence {
+    public static void main(String[] args) {
+        int[] nums={1,2,3,4,5};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+    }
+        public static int[] productExceptSelf(int[] nums) {
+            int zero=0,prod=1,l=nums.length;
+            int[] ans=new int[l];
+            for(int i=0;i<l;i++)
+            {
+                if(nums[i]!=0 )
+                    prod*=nums[i];
+                else{
+                    zero++;
+                    if(zero>1)
+                        return ans;
+                }
+            }
+            for(int i=0;i<l;i++)
+            {
+                if(zero==0)
+                    ans[i]=prod/nums[i];
+                if(zero==1 && nums[i]==0)
+                    ans[i]=prod;
+                // else
+                //     nums[i]=0;
+            }
+            return ans;
+        }
+    }
+
