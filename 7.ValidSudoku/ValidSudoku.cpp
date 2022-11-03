f126c8fe710a315315ad90c9b10de7100bb82993
phani-mithu <116943816+phani-mithu@users.noreply.github.com>
2022-11-03 16:36:00 +0530

    Update ValidSudoku.cpp

--- 7.ValidSudoku/ValidSudoku.cpp
+++ 7.ValidSudoku/ValidSudoku.cpp
@@ -1,9 +1,33 @@
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+      
+        vector<set<int>> row(9);
+        vector<set<int>> col(9);
+        vector<set<int>> box(9);
+      
+      
+        for(int i = 0 ; i < 9; i++){
+            for(int j = 0 ; j < 9 ; j++){
+              
+              
+                if(board[i][j] == '.'){
+                  continue;
+                }
+                
+                int curr = board[i][j];
+                int index = 3*(i/3) + (j/3);
+              
+                if(row[i].count(curr) || col[j].count(curr) || box[index].count(curr)){
+                   return false;
+                }
 
-
-int main(){
-  
-  return 0;
-}
+                row[i].insert(curr);
+                col[j].insert(curr);
+                box[index].insert(curr);
+                    
+            }
+        }
+        return true;
+    }
+};
